22984cfbcb69812be22f7e927546c6954694addc
RayoRodriguez <rayoara@hotmail.com>
2016-12-14 02:26:39 -0700

    Initial commit

---
+++ app/src/main/java/edu/tecii/android/intents/Manzana.java
@@ -0,0 +1,56 @@
+package edu.tecii.android.intents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;  // Se importan estas últimas librerías para poder realizar el intent.
+import android.widget.EditText;
+
+public class Manzana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manzana);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClick(View view){  // El método onClick para brincar al otro Intent.
+
+        Intent i = new Intent(this, Tocino.class);  // Crea el Intent.
+        final EditText txtMensaje = (EditText) findViewById(R.id.txtMensaje);   // Obtiene el texto inducido en "Manzanas".
+        String MensajeUsuario = txtMensaje.getText().toString();    //  Obtiene el Texto y lo convierte a String.
+        i.putExtra("MensajeManzana", MensajeUsuario);   //  Obtiene cualquier dato en forma de llave/valor.
+        // en este caso, nombre del String y valor.
+        startActivity(i);   // Comienza la clase.
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_manzana, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/edu/tecii/android/intents/Tocino.java
@@ -0,0 +1,45 @@
+package edu.tecii.android.intents;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.content.Intent;  // Se importan ambas últimas librerías para poder realizar el intent.
+import android.widget.TextView; // Este nos servirá para colocar el mensaje enviado al Texto en la actividad "Tocino".
+
+public class Tocino extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tocino);
+
+        Bundle InfoManzanas = getIntent().getExtras(); // Con este método se obtienen los extras mandados de la actividad que se envió.
+
+        if (InfoManzanas==null) {
+            return;     // Con esta condición delimitamos el envío de información desde otro recurso. Ej, desde otra actividad.
+        }
+
+        String MensajeManzana = InfoManzanas.getString("MensajeManzana");   // Se obtiene el texto que se escribió en "Manzanas".
+        final TextView txtTocino = (TextView) findViewById(R.id.txtTocino); // Se obtiene la referencia del TextView.
+        txtTocino.setText(MensajeManzana);  //  Se pone el texto que el usuario colocó en "MensajeManzana".
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClick(View view){  // El método onClick para brincar al otro Intent.
+        Intent i = new Intent(this, Manzana.class); // Los amo :)
+        startActivity(i);
+
+    }
+
+}
